22b947b67c2d29543766ae6625bc5e9aaefef3ff
Akhash-42 <malikabdel900@gmail.com>
2025-12-18 19:38:27 +0100

    ajout du runtime et du procfile avec quelque modification sur procfile

--- merchex/settings.py
+++ merchex/settings.py
@@ -17,7 +17,7 @@ os.environ.setdefault("PGDATABASE", "Merchex_dev")
 os.environ.setdefault("PGUSER", "postgres")
 os.environ.setdefault("PGPASSWORD", "12345")
 os.environ.setdefault("PGHOST", "localhost")
-os.environ.setdefault("PGPORT", "5432")
+os.environ.setdefault("PGPORT", 8000)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
